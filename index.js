6cf363064d62b731baef63d6ec5f9c28b7141b88
Vlad <shirker2006@gmail.com>
2024-11-14 17:04:42 -0800

    up

--- index.js
+++ index.js
@@ -3,10 +3,69 @@ const { handleStasisStart } = require('./src/stasisApp');
 
 // Start the ARI client connection
 connectToAri()
-    .then(() => console.log('ARI client connected'))
+    .then((ari) => {
+        console.log('ARI client connected')
+        ari.on('StasisStart', (event, channel) => {
+            const isHuman = parseInt(event.args[0]) === 1;  // First argument
+            let args = {};
+            try {
+                args = JSON.parse(event.args[1]);  // Second argument (appArgs JSON)
+            } catch (err) {
+                console.error('Failed to parse appArgs JSON:', err);
+                return;
+            }
+
+            const leadId = args.leadId;
+            const queueName = args.queueName;
+
+            console.log(`Channel ${channel.id} started in Stasis app`);
+            console.log(`isHuman: ${isHuman}, Lead ID: ${leadId}, Queue Name: ${queueName}`);
+        });
+
+        ari.on('ChannelHangupRequest', async (event, channel) => {
+            console.log(`Channel ${channel.id} has hung up`);
+      
+            try {
+                // Retrieve variables associated with the channel
+                const leadId = await channel.getChannelVar('LEADID');
+                const campaign = await channel.getChannelVar('CAMPAIGN');
+                const dspMode = await channel.getChannelVar('DSPMODE');
+                const isTransfer = await channel.getChannelVar('ISTRANSFER');
+                const hangupCause = event.cause;
+                if(!leadId || !campaign) {
+                    return;
+                }      
+                console.log(`Retrieved variables - LEADID: ${leadId}, CAMPAIGN: ${campaign}, DSPMODE: ${dspMode}, ISTRANSFER: ${isTransfer}`);
+        
+                // Update your database when a call is hung up
+                const [result] = await db.query(
+                    'DELETE FROM placed_calls WHERE lead_id = ?, campaign = ?',
+                    [leadId, campaign]
+                );
+                const leadTableName = 'campaign_' + campaign
+                let dispo = null;
+                switch(hangupCause) {
+                    case 'CONGESTION':
+                        dispo = -5;
+                        break;
+                    case 'NO_ANSWER':
+                        dispo = -2;
+                        break; 
+                    case 'BUSY':
+                        dispo = -4;
+                        break;
+                }
+                const [result2] = await db.query(
+                    'UPDATE ' + leadTableName + ' SET disposition = ? WHERE id = ?',
+                    [dispo, leadId]
+                );
+        
+                console.log('Database updated successfully for channel:', channel.id);
+            } catch (err) {
+                console.error('Error retrieving variables or updating database:', err);
+            }
+          });
+    
+    })
     .catch(err => console.error('Failed to start ARI client:', err));
 
-// Listen for StasisStart events and call the handler
-connectToAri.on('StasisStart', (event, channel) => {
-    handleStasisStart(channel);
-});
--- src/ariClient.js
+++ src/ariClient.js
@@ -16,68 +16,10 @@ async function connectToAri() {
         );
         console.log(`http://${ariHost}:${ariPort}`);
         const appName = config.get('ari.app_name') || 'gnudialer_stasis_app';
-        ari.on('StasisStart', (event, channel) => {
-            const isHuman = parseInt(event.args[0]) === 1;  // First argument
-            let args = {};
-            try {
-                args = JSON.parse(event.args[1]);  // Second argument (appArgs JSON)
-            } catch (err) {
-                console.error('Failed to parse appArgs JSON:', err);
-                return;
-            }
-
-            const leadId = args.leadId;
-            const queueName = args.queueName;
-
-            console.log(`Channel ${channel.id} started in Stasis app`);
-            console.log(`isHuman: ${isHuman}, Lead ID: ${leadId}, Queue Name: ${queueName}`);
-        });
-
-        ari.on('ChannelHangupRequest', async (event, channel) => {
-            console.log(`Channel ${channel.id} has hung up`);
-      
-            try {
-                // Retrieve variables associated with the channel
-                const leadId = await channel.getChannelVar('LEADID');
-                const campaign = await channel.getChannelVar('CAMPAIGN');
-                const dspMode = await channel.getChannelVar('DSPMODE');
-                const isTransfer = await channel.getChannelVar('ISTRANSFER');
-                const hangupCause = event.cause;
-                if(!leadId || !campaign) {
-                    return;
-                }      
-                console.log(`Retrieved variables - LEADID: ${leadId}, CAMPAIGN: ${campaign}, DSPMODE: ${dspMode}, ISTRANSFER: ${isTransfer}`);
-        
-                // Update your database when a call is hung up
-                const [result] = await db.query(
-                    'DELETE FROM placed_calls WHERE lead_id = ?, campaign = ?',
-                    [leadId, campaign]
-                );
-                const leadTableName = 'campaign_' + campaign
-                let dispo = null;
-                switch(hangupCause) {
-                    case 'CONGESTION':
-                        dispo = -5;
-                        break;
-                    case 'NO_ANSWER':
-                        dispo = -2;
-                        break; 
-                    case 'BUSY':
-                        dispo = -4;
-                        break;
-                }
-                const [result2] = await db.query(
-                    'UPDATE ' + leadTableName + ' SET disposition = ? WHERE id = ?',
-                    [dispo, leadId]
-                );
-        
-                console.log('Database updated successfully for channel:', channel.id);
-            } catch (err) {
-                console.error('Error retrieving variables or updating database:', err);
-            }
-          });
 
         ari.start(appName);
+
+        return ari
     } catch (err) {
         console.error('Failed to connect to ARI:', err);
     }
