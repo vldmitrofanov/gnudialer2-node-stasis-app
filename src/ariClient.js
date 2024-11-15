e3957088146056e22d586765a914640daaac38e2
Vlad <shirker2006@gmail.com>
2024-11-14 16:45:08 -0800

    wip

--- src/ariClient.js
+++ src/ariClient.js
@@ -1,16 +1,21 @@
 const AriClient = require('ari-client');
 const Config = require('./config');
 const config = new Config('/etc/gnudialer.conf');
+const db = require('./db');
 
 async function connectToAri() {
     try {
+        const ariPort = config.get('ari.ari_port') || 8088;
+        const ariHost = config.get('asterisk.master_host');
+        const ariUser = config.get('ari.ari_username');
+        const ariSecret = config.get('ari.ari_password');
         const ari = await AriClient.connect(
-            `http://${config.get('ARI.host')}:${config.get('ARI.port')}`,
-            config.get('ARI.username'),
-            config.get('ARI.password')
+            `http://${ariHost}:${ariPort}`,
+            ariUser,
+            ariSecret
         );
 
-        const appName = config.get('ARI.app');
+        const appName = config.get('ari.app_name') || 'gnudialer_stasis_app';
         ari.on('StasisStart', (event, channel) => {
             const isHuman = parseInt(event.args[0]) === 1;  // First argument
             let args = {};
@@ -27,6 +32,50 @@ async function connectToAri() {
             console.log(`Channel ${channel.id} started in Stasis app`);
             console.log(`isHuman: ${isHuman}, Lead ID: ${leadId}, Queue Name: ${queueName}`);
         });
+
+        ari.on('ChannelHangupRequest', async (event, channel) => {
+            console.log(`Channel ${channel.id} has hung up`);
+      
+            try {
+                // Retrieve variables associated with the channel
+                const leadId = await channel.getChannelVar('LEADID');
+                const campaign = await channel.getChannelVar('CAMPAIGN');
+                const dspMode = await channel.getChannelVar('DSPMODE');
+                const isTransfer = await channel.getChannelVar('ISTRANSFER');
+                const hangupCause = event.cause;
+                if(!leadId || !campaign) {
+                    return;
+                }      
+                console.log(`Retrieved variables - LEADID: ${leadId}, CAMPAIGN: ${campaign}, DSPMODE: ${dspMode}, ISTRANSFER: ${isTransfer}`);
+        
+                // Update your database when a call is hung up
+                const [result] = await db.query(
+                    'DELETE FROM placed_calls WHERE lead_id = ?, campaign = ?',
+                    [leadId, campaign]
+                );
+                const leadTableName = 'campaign_' + campaign
+                let dispo = null;
+                switch(hangupCause) {
+                    case 'CONGESTION':
+                        dispo = -5;
+                        break;
+                    case 'NO_ANSWER':
+                        dispo = -2;
+                        break; 
+                    case 'BUSY':
+                        dispo = -4;
+                        break;
+                }
+                const [result2] = await db.query(
+                    'UPDATE ' + leadTableName + ' SET disposition = ? WHERE id = ?',
+                    [dispo, leadId]
+                );
+        
+                console.log('Database updated successfully for channel:', channel.id);
+            } catch (err) {
+                console.error('Error retrieving variables or updating database:', err);
+            }
+          });
 
         ari.start(appName);
     } catch (err) {
--- src/db.js
+++ src/db.js
@@ -3,11 +3,11 @@ const Config = require('./config');
 const config = new Config('/etc/gnudialer.conf');
 
 const db = mysql.createPool({
-    host: config.get('MySQL.host'),
-    port: config.get('MySQL.port'),
-    user: config.get('MySQL.user'),
-    password: config.get('MySQL.password'),
-    database: config.get('MySQL.database')
+    host: config.get('database.mysql_host'),
+    port: config.get('database.mysql_port'),
+    user: config.get('database.mysql_username'),
+    password: config.get('database.mysql_password'),
+    database: config.get('database.mysql_dbname')
 });
 
 module.exports = db;
