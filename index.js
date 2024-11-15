1ffa208dffc9c08930102d5808736e5de6091d02
Vlad <shirker2006@gmail.com>
2024-11-15 12:09:48 -0800

    up

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ connectToAri()
         console.log('ARI client connected')
         ari.on('StasisStart', (event, channel) => {
             const isHuman = parseInt(event.args[0]) === 1;  // First argument
-            let args = {};
-            try {
-                args = JSON.parse(event.args[1]);  // Second argument (appArgs JSON)
-            } catch (err) {
-                console.error('Failed to parse appArgs JSON:', err);
-                return;
-            }
+            console.log('Is Human: ' + isHuman)
+            //let args = {};
+            //try {
+            //    args = JSON.parse(event.args[1]);  // Second argument (appArgs JSON)
+            //} catch (err) {
+            //    console.error('Failed to parse appArgs JSON:', err);
+            //    return;
+            //}
 
-            const leadId = args.leadId;
-            const queueName = args.queueName;
+            //const leadId = args.leadId;
+            //const queueName = args.queueName;
 
-            console.log(`Channel ${channel.id} started in Stasis app`);
-            console.log(`isHuman: ${isHuman}, Lead ID: ${leadId}, Queue Name: ${queueName}`);
+            //console.log(`Channel ${channel.id} started in Stasis app`);
+            //console.log(`isHuman: ${isHuman}, Lead ID: ${leadId}, Queue Name: ${queueName}`);
         });
 
         ari.on('ChannelHangupRequest', async (event, channel) => {
