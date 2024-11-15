62e1a7604c8d790adac7936eeda9c74877ab9ba2
Vlad <shirker2006@gmail.com>
2024-11-15 12:35:36 -0800

    up

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('./src/db');
 connectToAri()
     .then((ari) => {
         console.log('ARI client connected')
-        ari.on('StasisStart', async(event, channel) => {
+        ari.on('StasisStart', async (event, channel) => {
             const isHuman = parseInt(event.args[0]) === 1;  // First argument
             console.log('Is Human: ' + isHuman)
             //let args = {};
@@ -27,16 +27,16 @@ connectToAri()
                 const campaignResult = await ari.channels.getChannelVar({ channelId: channel.id, variable: 'CAMPAIGN' });
                 const dspModeResult = await ari.channels.getChannelVar({ channelId: channel.id, variable: 'DSPMODE' });
                 const isTransferResult = await ari.channels.getChannelVar({ channelId: channel.id, variable: 'ISTRANSFER' });
-        
+
                 const variables = {
                     leadId: leadIdResult.value,
                     campaign: campaignResult.value,
                     dspMode: dspModeResult.value,
                     isTransfer: isTransferResult.value,
                 };
-        
+
                 console.log('Storing variables for channel:', channel.id, variables);
-        
+
                 // Store variables in the global Map
                 channelVariables.set(channel.id, variables);
             } catch (err) {
@@ -54,11 +54,11 @@ connectToAri()
                     console.error(`No stored variables found for channel: ${channel.id}`);
                     return;
                 }
-        
+
                 console.log('Retrieved variables from global Map:', variables);
-        
+
                 const { leadId, campaign, dspMode, isTransfer } = variables;
-        
+
                 if (!leadId || !campaign) {
                     console.error('Missing required variables: LEADID or CAMPAIGN');
                     return;
@@ -69,6 +69,8 @@ connectToAri()
                     [leadId, campaign]
                 );
                 const leadTableName = 'campaign_' + campaign
+                const hangupCause = event.cause || 'UNKNOWN';
+                console.log('Hangup cause:', hangupCause);
                 let dispo = null;
                 switch (hangupCause) {
                     case 'CONGESTION':
@@ -80,13 +82,18 @@ connectToAri()
                     case 'BUSY':
                         dispo = -4;
                         break;
+                    case 'UNKNOWN':
+                        dispo = -1
+                        break;
                 }
-                const [result2] = await db.query(
-                    'UPDATE ' + leadTableName + ' SET disposition = ? WHERE id = ?',
-                    [dispo, leadId]
-                );
+                if (dispo !== null) {
+                    const [result2] = await db.query(
+                        'UPDATE ' + leadTableName + ' SET disposition = ? WHERE id = ?',
+                        [dispo, leadId]
+                    );
 
-                console.log('Database updated successfully for channel:', channel.id);
+                    console.log('Database updated successfully for channel:', channel.id);
+                }
             } catch (err) {
                 console.error('Error retrieving variables or updating database:', err);
             }
