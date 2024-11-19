c16041a4dfd0e7886b750435f1f87776435980a1
Vlad <shirker2006@gmail.com>
2024-11-19 14:34:49 -0800

    up

--- src/ORM/createOrFindBridge.js
+++ src/ORM/createOrFindBridge.js
@@ -9,7 +9,7 @@ async function createOrFindBridge(agentId, serverId) {
 
         let bridgeRecord = existingBridge || null;
 
-        if (!bridgeRecord) {
+        if (!bridgeRecord || bridgeRecord.length===0) {
             // No bridge found, create a new entry in the database
             const [insertResult] = await db.query(
                 'INSERT INTO conf_bridges (agent_id, server_id, online, available, pause, created_at, updated_at) VALUES (?, ?, 0, 1, 0, NOW(), NOW())',
@@ -25,7 +25,7 @@ async function createOrFindBridge(agentId, serverId) {
         }
         console.log('bridgeRecord', bridgeRecord)
 
-        const bridgeName = bridgeRecord.id.toString();
+        const bridgeName = bridgeRecord[0].id.toString();
 
         return bridgeName;
 
