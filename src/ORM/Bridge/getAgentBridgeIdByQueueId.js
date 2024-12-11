b44fa5cf0cb99f9b94db5d35960f4d7c1f3e7bc7
Vlad <shirker2006@gmail.com>
2024-12-10 16:34:02 -0800

    up

--- src/ORM/Bridge/getAgentBridgeIdByQueueId.js
+++ src/ORM/Bridge/getAgentBridgeIdByQueueId.js
@@ -1,13 +1,13 @@
 const db = require('../../db');
-async function getAgentBridgeId(campaign, serverId) {
+async function getAgentBridgeIdByQueueId(queueId) {
     let query = "SELECT conf_bridges.id FROM conf_bridges "
     query +="LEFT JOIN agent_queue ON conf_bridges.agent_id = agent_queue.agent_id "
                 "LEFT JOIN queues ON agent_queue.queue_id = queues.id "
-                "LEFT JOIN campaigns ON queues.campaign_id = campaigns.id "
-                "WHERE campaigns.code = ? AND conf_bridges.server_id = ? "
-                "AND conf_bridges.online = 1 AND conf_bridges.available = 1 AND conf_bridges.pause = 0 ORDER BY conf_bridges.updated_at ASC LIMIT 1"
+                "WHERE agent_queue.queue_id = ? "
+                "AND conf_bridges.online = 1 AND conf_bridges.available = 1 "
+                "AND conf_bridges.pause = 0 ORDER BY conf_bridges.updated_at ASC LIMIT 1"
     const [rows] = await db.query(query,
-        [campaign,serverId]
+        [queueId]
     );
     if (rows.length > 0) {
         console.log(`Found bridge ID for campaign ${campaign}: ${rows[0].id}`);
@@ -17,4 +17,4 @@ async function getAgentBridgeId(campaign, serverId) {
         return null;
     }
 }
-module.exports = getAgentBridgeId;
+module.exports = getAgentBridgeIdByQueueId;
--- src/runInbound.js
+++ src/runInbound.js
@@ -1,4 +1,5 @@
-const createOrFindBridge = require('./ORM/Bridge/createOrFindBridge')
+const createOrFindBridge = require('./ORM/Bridge/createOrFindBridge');
+const getAgentBridgeIdByQueueId = require('./ORM/Bridge/getAgentBridgeIdByQueueId');
 const updateBridge = require('./ORM/Bridge/updateBridge');
 const getInboundByNumber = require('./ORM/Inbound/getInboundByNumber');
 
@@ -9,35 +10,46 @@ const runAgentJoinBridge = async ({
     serverId
 }) => {
     try {
-            // Retrieve the EXTEN variable to get the dialed number
-            const dialedNumber = await ari.channels.getChannelVar({ 
-                channelId: channel.id, 
-                variable: 'EXTEN' 
-            });
-        const inbound = await getInboundByNumber(dialedNumber, serverId);
-        /* const bridges = await ari.bridges.list();
-        let ariBridge = bridges.find((bridge) => bridge.name === bridgeName);
-
-        if (!ariBridge) {
-            // Create a new ARI bridge
-            ariBridge = await ari.bridges.create({
-                type: 'mixing',
-                name: bridgeName,
-            });
-        }
-        await ari.bridges.addChannel({
-            bridgeId: ariBridge.id,
-            channel: channel.id,
+        // Retrieve the EXTEN variable to get the dialed number
+        const dialedNumber = await ari.channels.getChannelVar({
+            channelId: channel.id,
+            variable: 'EXTEN'
         });
-        await updateBridge({
-            id: bridgeName,
-            bridgeId: ariBridge.id,
-            online: 1,
-            available: 1,
-            pause: 0,
-            serverId: serverId
-        }) */
-        //console.log(`Database updated for bridge ID: ${bridgeName}`);
+        const inbound = await getInboundByNumber(dialedNumber, serverId);
+        if (inbound['routable_type'] === 'App\\Models\\Queue') {
+            console.log('This is a Queue model.');
+            let availableBridgeName = getAgentBridgeIdByQueueId(inbound['routable_id'])
+
+            if (!availableBridgeName) {
+                // here we run loop
+                // and wait for available agent
+            }
+
+            const bridges = await ari.bridges.list();
+            let ariBridge = bridges.find((bridge) => bridge.name === availableBridgeName);
+
+            if (!ariBridge) {
+                // Create a new ARI bridge
+                ariBridge = await ari.bridges.create({
+                    type: 'mixing',
+                    name: availableBridgeName,
+                });
+            }
+            await ari.bridges.addChannel({
+                bridgeId: ariBridge.id,
+                channel: channel.id,
+            });
+            await updateBridge({
+                id: availableBridgeName,
+                bridgeId: ariBridge.id,
+                online: 1,
+                available: 1,
+                pause: 0,
+                serverId: serverId
+            })
+            //console.log(`Database updated for bridge ID: ${availableBridgeName}`);
+        }
+
     } catch (err) {
         console.error('Error in runAgentJoinBridge:', err);
     }
